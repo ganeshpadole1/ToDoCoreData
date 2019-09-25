3d884aae1063340b846bcea7633f95dd7347875f
ganesh padole <ganeshpadole55@gmail.com>
2019-09-25 23:19:44 +0530

    Initial Commit

---
+++ ToDoCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoCoreData
+//
+//  Created by ganesh padole on 03/07/1941 Saka.
+//  Copyright © 1941 ganesh padole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoCoreDataTests/ToDoCoreDataTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoCoreDataTests.swift
+//  ToDoCoreDataTests
+//
+//  Created by ganesh padole on 03/07/1941 Saka.
+//  Copyright © 1941 ganesh padole. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoCoreData
+
+class ToDoCoreDataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
